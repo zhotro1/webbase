6a7cbb51c3c7d5a583f058fc887c9f6a496024fe
NgocHieu <zhotro1@gmail.com>
2020-08-26 08:00:05 +0700

    fix error

--- baseapp/wsgi.py
+++ baseapp/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 project_name = os.getcwd().split('/')[-1]
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{project_name}.settings')
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
